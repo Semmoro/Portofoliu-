2bf7122c5d91f7590bcc3a6cf1026c1f9e06006a
Catalin Morozan <semmoro0.3@gmail.com>
2025-08-23 14:52:08 +0300

    git add

--- dev-lofthouse/src/main-lofthouse.js
+++ dev-lofthouse/src/main-lofthouse.js
@@ -1,3 +1,7 @@
+import '../../dev-main/src/reset.css'
+import './lofthouse.css'
+import './media-lofthouse.css'
+
 import {burgerMenuButton} from './burger-menu.js'
 import {slider} from './slider.js'
 
